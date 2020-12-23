2d2e902d9cf6f2ae8104fd68fca6701de997bf03
insta <insta@DESKTOP-IAVPL5C>
2020-12-23 21:09:38 +0900

    미완성

---
+++ Practice/practice05.sql
@@ -0,0 +1,158 @@
+/****
+practice05
+****/
+
+/*문제1.
+담당 매니저가 배정되어있으나 커미션비율이 없고, 월급이 3000초과인 직원의 
+이름, 매니저아이디, 커미션 비율, 월급 을 출력하세요.
+(45건)
+*/
+select  first_name,
+        manager_id,
+        commission_pct,
+        salary
+from employees
+where commission_pct is null
+and manager_id is not null
+and salary > 3000;
+
+/*
+문제2. 
+각 부서별로 최고의 급여를 받는 사원의 직원번호(employee_id),
+이름(first_name), 급여(salary), 입사일(hire_date), 
+전화번호(phone_number), 부서번호(department_id) 를 조회하세요 
+-조건절비교 방법으로 작성하세요
+-급여의 내림차순으로 정렬하세요
+-입사일은 2001-01-13 토요일 형식으로 출력합니다.
+-전화번호는 515-123-4567 형식으로 출력합니다.
+(11건)
+*/
+select  department_id,
+        max(salary)
+from employees
+group by department_id;
+
+select  employee_id 직원번호,
+        first_name 이름,
+        salary 급여,
+        to_char(hire_date,'yyyy-mm-dd day') 입사일,
+        replace(phone_number,'.','-') 전화번호,
+        department_id 부서번호
+from employees
+where (department_id, salary) in (select  department_id,
+                                          max(salary)
+                                  from employees
+                                  group by department_id);
+
+/*문제3
+매니저별로 평균급여 최소급여 최대급여를 알아보려고 한다.
+-통계대상(직원)은 2005년 이후의 입사자 입니다.
+-매니저별 평균급여가 5000이상만 출력합니다.
+-매니저별 평균급여의 내림차순으로 출력합니다.
+-매니저별 평균급여는 소수점 첫째자리에서 반올림 합니다.
+-출력내용은 매니저아이디, 매니저이름(first_name), 매니저별평균급여,
+매니저별최소급여, 매니저별최대급여 입니다.
+(9건)
+*/
+select  manager_id,
+        hire_date,
+        salary,
+        avg(salary)
+from employees emp
+group by manager_id, hire_date, salary
+having hire_date >= '05/01/01'
+and salary >= 5000;
+
+select  avgs.manager_id,
+        avgs.first_name,
+        avgs.hire_date,
+        avgs.salary
+from employees emp, employees emp2, (select  manager_id,
+                                             hire_date,
+                                             salary,
+                                             first_name,
+                                             avg(salary)
+                                     from employees emp
+                                     group by manager_id, first_name, hire_date, salary
+                                     having hire_date >= '05/01/01'
+                                     and salary >= 5000) avgs
+where emp.department_id = emp2.manager_id;                                   
+
+/*문제4.
+각 사원(employee)에 대해서 사번(employee_id), 이름(first_name),
+부서명(department_name), 매니저(manager)의 이름(first_name)을 조회하세요.
+부서가 없는 직원(Kimberely)도 표시합니다.
+(106명)
+*/
+
+select  emp.employee_id,
+        emp.first_name,
+        dep.department_name,
+        emp.first_name
+from employees emp left outer join departments dep
+on emp.department_id = dep.department_id, employees emp2
+where emp.manager_id = emp2.employee_id;
+
+/*문제5.
+2005년 이후 입사한 직원중에 입사일이 11번째에서 20번째의 직원의 
+사번, 이름, 부서명, 급여, 입사일을 입사일 순서로 출력하세요
+*/
+select  ro.rn,
+        ro.employee_id,
+        ro.first_name,
+        dep.department_name,
+        ro.salary,
+        ro.hire_date
+from departments dep,(select  rownum rn,
+                     emh.employee_id,
+                     emh.first_name,
+                     emh.department_id,
+                     emh.salary,
+                     emh.hire_date
+             from (select  employee_id,
+                           first_name,
+                           department_id,
+                           salary,
+                           hire_date
+                   from employees 
+                   where hire_date > '2005/12/31'
+                   order by hire_date asc)emh
+      )ro
+where ro.rn >= 11
+and ro.rn <= 20
+and ro.department_id = dep.department_id
+order by ro.rn asc;
+
+/*문제6
+가장 늦게 입사한 직원의 이름(first_name last_name)과 연봉(salary)과
+근무하는 부서 이름(department_name)은?
+*/
+select  max(hire_date)
+from employees;
+
+select  em.first_name || ' ' || em.last_name,
+        em.salary,
+        em.hire_date,
+        de.department_name
+from employees em, departments de
+where em.department_id = de.department_id
+and em.hire_date = (select  max(hire_date)
+                    from employees);
+                    
+/*문제7.
+평균연봉(salary)이 가장 높은 부서 직원들의 직원번호(employee_id),
+이름(firt_name), 성(last_name)과  업무(job_title),
+연봉(salary)을 조회하시오.
+*/
+select  max(avg(salary))
+from employees;
+
+select  em.employee_id,
+        em.first_name,
+        em.last_name,
+        jo.job_title,
+        em.salary
+from employees em, jobs jo
+where em.job_id = jo.job_id
+and em.salary <= (select  max(avg(salary))
+                  from employees)
